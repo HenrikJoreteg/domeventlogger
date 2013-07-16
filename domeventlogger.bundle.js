a31bc9f949c2d97c21abe6d68f35f2125daf8973
Henrik Joreteg <hjoreteg@gmail.com>
2013-07-16 00:02:52 -0700

    update to use add event listener, duh\!

--- domeventlogger.bundle.js
+++ domeventlogger.bundle.js
@@ -2,17 +2,17 @@
 return (function(e,t,n){function i(n,s){if(!t[n]){if(!e[n]){var o=typeof require=="function"&&require;if(!s&&o)return o(n,!0);if(r)return r(n,!0);throw new Error("Cannot find module '"+n+"'")}var u=t[n]={exports:{}};e[n][0].call(u.exports,function(t){var r=e[n][1][t];return i(r?r:t)},u,u.exports)}return t[n].exports}var r=typeof require=="function"&&require;for(var s=0;s<n.length;s++)i(n[s]);return i})({1:[function(require,module,exports){
 module.exports = function (element) {
     Object.getOwnPropertyNames(element).forEach(function (item) {
+        var eventName;
         if (item.indexOf('on') === 0) {
-            var existing = element[item];
-            element[item] = function (event) {
+            eventName = item.slice(2);
+            element.addEventListener(eventName, function (event) {
                 var label = element.tagName;
                 if (element.id) label += '#' + element.id;
                 if (element.className) label += element.className.split(' ').map(function (cls) {
                     return '.' + cls;
                 }).join('');
                 console.log(label, 'name: ' + item.slice(2), event);
-                if (existing) existing();
-            };
+            }, false);
         }
     });
 
--- domeventlogger.js
+++ domeventlogger.js
@@ -1,16 +1,16 @@
 module.exports = function (element) {
     Object.getOwnPropertyNames(element).forEach(function (item) {
+        var eventName;
         if (item.indexOf('on') === 0) {
-            var existing = element[item];
-            element[item] = function (event) {
+            eventName = item.slice(2);
+            element.addEventListener(eventName, function (event) {
                 var label = element.tagName;
                 if (element.id) label += '#' + element.id;
                 if (element.className) label += element.className.split(' ').map(function (cls) {
                     return '.' + cls;
                 }).join('');
                 console.log(label, 'name: ' + item.slice(2), event);
-                if (existing) existing();
-            };
+            }, false);
         }
     });
 
